21e8c21cf006f2df199a1070b8ba92cbb774b731
akbar <sdakbar4u@gmail.com>
2019-06-11 16:50:42 +0530

    aa

---
+++ src/main/java/com/app/SpringBootJpaExApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpaExApplication.class, args);
+	}
+
+}
